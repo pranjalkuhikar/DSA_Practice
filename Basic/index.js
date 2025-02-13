7e9f4057a18f31c05e106e592c90b35789b911ed
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-13 19:26:12 +0530

    Update index.js to prompt user for name and age instead of summing two numbers

--- Basic/index.js
+++ Basic/index.js
@@ -8,6 +8,10 @@ const prompt = require("prompt-sync")();
 // let b = 3;
 // console.log(`The Sum of ${a} and ${b} is ${a + b}`);
 
-let a = Number(prompt("Enter the First Number :- "));
-let b = Number(prompt("Enter the Second Number :- "));
-console.log(`The Sum of ${a} and ${b} is ${a + b}`);
+// let a = Number(prompt("Enter the First Number :- "));
+// let b = Number(prompt("Enter the Second Number :- "));
+// console.log(`The Sum of ${a} and ${b} is ${a + b}`);
+
+let name = prompt("Enter Your Name :- ");
+let age = Number(prompt("Enter Your Age :- "));
+console.log(`My name is ${name} and My age is ${age}`);
