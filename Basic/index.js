0c7fdbce36b30a5267d8afd1d81824dcfc52124f
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-13 19:22:11 +0530

    Refactor index.js to log the sum of two variables instead of a static calculation

--- Basic/index.js
+++ Basic/index.js
@@ -2,4 +2,8 @@ const prompt = require("prompt-sync")();
 
 // console.log("Hello World");
 
-console.log(5 + 5);
+// console.log(5 + 5);
+
+let a = 4;
+let b = 3;
+console.log(`The Sum of ${a} and ${b} is ${a + b}`);
