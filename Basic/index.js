0b64cb316ad04d5284856f5c86a2256456096b3d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-13 19:24:39 +0530

    Update index.js to prompt user for input and log the sum of two numbers

--- Basic/index.js
+++ Basic/index.js
@@ -4,6 +4,10 @@ const prompt = require("prompt-sync")();
 
 // console.log(5 + 5);
 
-let a = 4;
-let b = 3;
+// let a = 4;
+// let b = 3;
+// console.log(`The Sum of ${a} and ${b} is ${a + b}`);
+
+let a = Number(prompt("Enter the First Number :- "));
+let b = Number(prompt("Enter the Second Number :- "));
 console.log(`The Sum of ${a} and ${b} is ${a + b}`);
