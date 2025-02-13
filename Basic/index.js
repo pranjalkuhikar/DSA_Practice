2f93de093e954861c3df8b92c5f8cb3dd518954d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-13 19:30:58 +0530

    Update index.js to comment out user prompts and add rectangle perimeter and area calculations

--- Basic/index.js
+++ Basic/index.js
@@ -1,5 +1,7 @@
 const prompt = require("prompt-sync")();
 
+// -----------------------------------Basic-----------------------------------
+
 // console.log("Hello World");
 
 // console.log(5 + 5);
@@ -12,6 +14,13 @@ const prompt = require("prompt-sync")();
 // let b = Number(prompt("Enter the Second Number :- "));
 // console.log(`The Sum of ${a} and ${b} is ${a + b}`);
 
-let name = prompt("Enter Your Name :- ");
-let age = Number(prompt("Enter Your Age :- "));
-console.log(`My name is ${name} and My age is ${age}`);
+// let name = prompt("Enter Your Name :- ");
+// let age = Number(prompt("Enter Your Age :- "));
+// console.log(`My name is ${name} and My age is ${age}`);
+
+// -----------------------------------Formula-----------------------------------
+let length = 3;
+let breadth = 2;
+console.log(`Perimeter of Rectangle is ${2 * (length + breadth)} `);
+console.log(`Area of Rectangle id ${length * breadth}`);
+
