d0b889527feae5d9bbc61d318e0e818912311c3d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-15 21:20:07 +0530

    Implement check for Abundant number in the code

--- Basic/index.js
+++ Basic/index.js
@@ -646,3 +646,14 @@ const prompt = require("prompt-sync")();
 //     ? "Automorphic Number"
 //     : "Not a Automorphic NUmber"
 // );
+
+let n = 12;
+let temp = n;
+let sum = 0;
+for (let i = 1; i < n; i++) {
+  if (n % i === 0) {
+    sum += i;
+  }
+}
+
+console.log(temp < sum ? "Abundant Number" : "Not a Abundant Number");
