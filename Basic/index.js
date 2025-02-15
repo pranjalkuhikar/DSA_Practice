1e7143ec42c49abafc57a4a7148ed981bfc3f1df
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-15 20:50:43 +0530

    Refactor code to comment out FizzBuzz logic

--- Basic/index.js
+++ Basic/index.js
@@ -528,17 +528,17 @@ const prompt = require("prompt-sync")();
 //   a = temp;
 // }
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
 // Question 50: Reverse a number.
 // let n = 1234;
