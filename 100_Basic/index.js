6c6571400a552dea42647b727799deeed02ad6dd
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-16 17:31:47 +0530

    Refactor number classification logic; comment out positive/negative check and add even/odd check

--- 100_Basic/index.js
+++ 100_Basic/index.js
@@ -1,8 +1,15 @@
 const prompt = require("prompt-sync")();
 
-let n = 5;
-if (n >= 0) {
-  console.log("Positive Number");
+// let n = 5;
+// if (n >= 0) {
+//   console.log("Positive Number");
+// } else {
+//   console.log("Negative Number");
+// }
+
+let n = 8;
+if (n % 2 === 0) {
+  console.log("Even Number");
 } else {
-  console.log("Negative Number");
+  console.log("Odd Number");
 }
