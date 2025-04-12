a0b5dd2de1f18b7c372577c9b1cc7f4a4894a136
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-04-12 13:26:40 +0530

    Comment out prime number function and related code; add comments for clarity

--- Basic/index1.js
+++ Basic/index1.js
@@ -52,3 +52,53 @@ let n = 23;
 //   }
 // }
 // console.log(count === 2 ? "P" : "NP");
+
+// function primeNumber(n) {
+//   let count = 0;
+//   for (let i = 0; i <= Math.floor(Math.sqrt(n)); i++) {
+//     if (n % i === 0) {
+//       count++;
+//       if (n / i != i) {
+//         count++;
+//       }
+//     }
+//   }
+//   return count === 2;
+// }
+
+// for (let i = 1; i <= 500; i++) {
+//   if (primeNumber(i)) {
+//     console.log(i);
+//   }
+// }
+
+// let sum = 0;
+// while (n != 0) {
+//   let lastDigit = n % 10;
+//   sum += lastDigit;
+//   n = Math.floor(n / 10);
+// }
+// console.log(sum);
+
+// let a = 0;
+// let b = 1;
+// for (let i = 1; i <= n; i++) {
+//   console.log(a);
+//   let temp = a + b;
+//   a = b;
+//   b = temp;
+// }
+
+// let a = 12;
+// let b = 20;
+// let temp1 = a;
+// let temp2 = b;
+// while (b != 0) {
+//   let temp = b;
+//   b = a % b;
+//   a = temp;
+// }
+// console.log(a);
+// let lcm = (temp1 * temp2) / a;
+// console.log(lcm);
+
