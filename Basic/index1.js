19b4aea23275720579e2b33154153977fb9610a9
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-04-12 11:37:05 +0530

    Comment out various functions for clarity and organization

---
+++ Basic/index1.js
@@ -0,0 +1,54 @@
+let n = 23;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// let fact = 1;
+// for (let i = 1; i <= n; i++) {
+//   fact *= i;
+// }
+// console.log(fact);
+
+// for (let i = 0; i <= n; i++) {
+//   if (n % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+// let even = 0;
+// let odd = 0;
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 === 0) even += i;
+//   else odd += i;
+// }
+// console.log(even, odd);
+
+// let base = 2;
+// let expo = 4;
+// let sum = 1;
+// for (let i = 1; i <= expo; i++) {
+//   sum *= base;
+// }
+// console.log(sum);
+
+// let count = 0;
+// for (let i = 0; i <= Math.floor(Math.sqrt(n)); i++) {
+//   if (n % i === 0) {
+//     count++;
+//     if (n / i != i) {
+//       count++;
+//     }
+//   }
+// }
+// console.log(count === 2 ? "P" : "NP");
