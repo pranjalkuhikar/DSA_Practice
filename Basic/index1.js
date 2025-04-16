b0969adbd2ca4050f7f79d13c9fb58af6b27cb08
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-04-16 09:26:49 +0530

    Comment out additional array manipulation examples for clarity and organization; enhance code readability

--- Basic/index1.js
+++ Basic/index1.js
@@ -739,3 +739,23 @@
 //   }
 // }
 // console.log(newArr);
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// let newArr = Array.from({ length: arr[0].length }, () => Array(arr.length));
+// for (let i = 0; i < newArr.length; i++) {
+//   for (let j = 0; j < newArr[i].length; j++) {
+//     newArr[i][j] = arr[j][i];
+//   }
+// }
+// console.log(newArr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i; j < arr[i].length; j++) {
+//     [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]];
+//   }
+// }
+// console.log(arr);
